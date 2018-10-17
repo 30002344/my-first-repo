d826f49dd9271c1036a4f2ce6dfcf7acfd20f78b
Patrick Crampton <30002344@student.toiohomai.ac.nz>
2018-10-18 09:56:26 +1300

    this is my temp file

---
+++ winlistbox/winlistbox/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winlistbox
+{
+    public partial class Form1 : Form
+    {
+               
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstNames.Items.Add(txtName.Text);
+            txtName.Clear();
+            txtName.Focus();
+             
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtName.Focus();
+        }
+
+        private void lstNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
